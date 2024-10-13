6ec59a89169ae91f191d0fb10ce610c2fded1b68
AG <agrewal4@my.yorku.ca>
2024-10-12 23:54:11 -0400

    Removed previous setup

--- doc/sprint0/E-Commerce_Project/web_app/members/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render           # added this (w3)
-from django.http import HttpResponse          # added this (w3)
-
-# Create your views here.
-
-def members(request):                          # added this (w3)
-    return HttpResponse("Hello BargainBox Customer!")     # added this (w3)
