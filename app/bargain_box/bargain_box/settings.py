8992235982dce70ccb1e141d12ec5eeb836332a6
mikhailF01 <mikef01+gh@my.yorku.ca>
2024-11-02 15:41:07 -0400

    Added styles and some other minor improvements.

--- app/bargain_box/bargain_box/settings.py
+++ app/bargain_box/bargain_box/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    'bargain_box/static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
