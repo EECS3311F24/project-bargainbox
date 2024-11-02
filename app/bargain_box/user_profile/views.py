d5ad1478d58b28a399c39e29397d3f437623e0ab
timyorku <tim17@my.yorku.ca>
2024-11-02 09:54:38 -0400

    Update user profile view function

--- app/bargain_box/user_profile/views.py
+++ app/bargain_box/user_profile/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+from django.urls import reverse
+
+
 # Create your views here.
+@login_required
 def view_profile(request):
     context = {
-        'page_title': 'My profile'
+        'page_title': 'My profile',
     }
+
     return render(request, 'user_profile/view_profile.html', context)
 
 def edit_profile(request):
