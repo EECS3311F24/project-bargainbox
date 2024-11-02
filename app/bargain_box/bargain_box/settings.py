322f11a524305e91a487299940902a7e01cccae7
AG <agrewal4@my.yorku.ca>
2024-11-02 13:17:22 -0400

    Created the Login and Logout System
    The Login and Logout system was created and any references to/from the Registration system have been updated.

--- app/bargain_box/bargain_box/settings.py
+++ app/bargain_box/bargain_box/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+################################# Used to Redirect Onced Signed In
+LOGIN_REDIRECT_URL = 'home'
--- app/bargain_box/user_authentication/views.py
+++ app/bargain_box/user_authentication/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+############################################ added (needed for sign out page to work correctly)
+from django.contrib.auth import logout
+
 # Create your views here.
 def user_account_registration(request):
     context = {
@@ -14,8 +17,13 @@ def user_account_signin(request):
     }
     return render(request, 'user_authentication/signin.html', context)
 
+#def user_account_signout(request):
+#    context = {
+#        'page_title': 'Signout'
+#    }
+#    return render(request, 'user_authentication/signout.html', context)
+
+########################################### added (needed for sign out page to work correctly)
 def user_account_signout(request):
-    context = {
-        'page_title': 'Signout'
-    }
-    return render(request, 'user_authentication/signout.html', context)
+    logout(request)
+    return render(request, 'user_authentication/signout.html')
