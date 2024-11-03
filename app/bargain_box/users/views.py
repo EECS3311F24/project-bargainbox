a123d7194e65bd24a461a6e10eb40e99cd27fd5a
Annie Jhamb <123187980+Anniejhamb15@users.noreply.github.com>
2024-11-02 20:14:42 -0400

    commit changes

--- app/bargain_box/users/views.py
+++ app/bargain_box/users/views.py
@@ -9,31 +9,26 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You will now be able to Login!')       ### updated
-            return redirect('signin')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')
     else:
         form = UserRegisterForm()
     
     return render(request, 'users/register.html', {'form': form})
-    
-def profile(request):
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        
-        if u_form.is_valid() and p_form.is_valid():  
-            u_form.save()
-            p_form.save()            
-        messages.success(request, f'Your account has been updated!')       ### updated
-        return redirect('profile')
 
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+@login_required
+def profiel(request):
+    u_form = UserUpdateForm()
+    p_form = ProfileUpdateForm()
 
     context = {
-            'u_form': u_form,
-            'p_form': p_form
-        }
+        'u_form': u_form,
+        'p_form': p_form
+    }
 
-    return render (request, 'users/profile.html', context)
+    return render (request,'user_profile/view_profile.html',context)
+
+
+
+    
