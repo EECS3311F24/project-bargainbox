746ab539ef66b3df5ab3cce3929fe41f3d7a19b8
AG <agrewal4@my.yorku.ca>
2024-11-08 21:52:39 -0500

    Allow Signed in Users to Create Posts
    Ticket #1: Backend Setup for Posts Completed. Tested so that posts can be manually added by admin users. Updated CSS so that Posts on Homepage look cleaner. Added an HTML Link to the Post Title so that the Detailed Post View feature could be implemented.

--- app/bargain_box/home/views.py
+++ app/bargain_box/home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# import added
+from .models import Post
+
 # Create your views here.
 def home_page(request):
     context = {
-        'page_title': 'Home'
+        'posts': Post.objects.all()
     }
     return render(request, 'home/home.html', context)
