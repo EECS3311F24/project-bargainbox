5104e72fde6a798f6fbfa3577a34a145ff3ec741
timyorku <tim17@my.yorku.ca>
2024-11-09 17:54:57 -0500

    Update gitignore to not include __pycache__, create a new app called Post and move anything related to Post to the new app

--- app/bargain_box/home/models.py
+++ app/bargain_box/home/models.py
@@ -1,27 +1,3 @@
-
-# imports
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
-
-from django.urls import reverse
 
 # Create your models here.
-
-# attributes for Post
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    location = models.CharField(max_length=100)
-    quantity = models.PositiveIntegerField(default=1)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    date_posted = models.DateTimeField(default = timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='post_images/', null=True, blank=True) # Allows pictures to be added to posts (pictures are optional)
-
-    def __str__(self):
-        return self.title
-
-    # So that user is redirected to homepage once post is created
-    def get_absolute_url(self):
-        return reverse('home')
