39203312d5bd359c0feceb6c9443ecdae6f3d1aa
mikhailF01 <mikef01+gh@my.yorku.ca>
2024-11-23 10:38:25 -0500

    Updated user profile related routes and views

--- app/bargain_box/user_profile/urls.py
+++ app/bargain_box/user_profile/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('view/', views.view_profile, name = 'view-profile'),
+    path('view/', views.view_profile, name = 'view-profile'), # used to view the signed in user's profile
+    path('view/<int:pk>/', views.view_profile, name = 'post-detail-given-user-pk'), # used to view a non-signed in user's profile
     path('edit/', views.edit_profile, name = 'edit-profile'),
     path('delete/', views.delete_profile, name = 'delete-profile')
 ]
--- app/bargain_box/user_profile/views.py
+++ app/bargain_box/user_profile/views.py
@@ -6,16 +6,49 @@ from django.contrib.auth.decorators import login_required
 from .forms import UpdateUserTableForm, UpdateProfileTableForm
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
+from post.models import Post
 
 # Create your views here.
-def view_profile(request):
+def view_profile(request, pk = None):
     # Check to make sure user is logged in
     if request.user.is_authenticated:
         context = {
-            'page_title': 'My profile',
+            'page_title': None,
+            'user_to_view': None,
+            'user_active_listings': 0
         }
 
+        # if a primary key is not provided in the URL, that means the user wants to view their own profile.
+        # if a primary key is provided in the URL, that means the user wants to view another user's profile.
+        if pk == None:
+            context['page_title'] = 'My profile'
+            context['user_to_view'] = request.user
+            context['user_active_listings'] = Post.objects.filter(author=request.user.pk).count()
+        else:
+            # get all users with a primary key that matches the primary key encoded in the request's URL.
+            user_to_view = User.objects.filter(pk=pk)
+
+            # if the number of users with a matching primary key is not equal to one, the request is invalid.
+            if user_to_view.count() != 1:
+                return HttpResponse('ERROR - PK does not exist')
+                #error - bad request, implement later
+            
+            user_to_view = user_to_view.first()
+            # get the number of listings that belong to the target user.
+            user_active_listings = Post.objects.filter(author=user_to_view.pk).count()
+            
+            # If the number of listings is 0, then the user's profile should be inaccassible for privacy reasons.
+            # If the user has 0 listings, there is no reason for their profile to be publically accessible by other users.
+            if user_active_listings == 0:
+                return HttpResponse('ERROR - target user has no posts')
+                #error - bad request, implement later
+
+            context['page_title'] = 'User profile'
+            context['user_to_view'] = user_to_view
+            context['user_active_listings'] = user_active_listings
+        
         return render(request, 'user_profile/view_profile.html', context)
+
     else:
         # Not logged in so redirect to home page
         return HttpResponseRedirect(reverse('home'))
