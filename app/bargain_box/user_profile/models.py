f08c6dc7e78821e651effde29bb0264c9075b619
mikhailF01 <mikef01+gh@my.yorku.ca>
2024-11-24 19:32:20 -0500

    Implemented view seller's profile among others.

--- app/bargain_box/user_profile/models.py
+++ app/bargain_box/user_profile/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='profile_images', default='profile_images/default.png')
+    biography = models.TextField(max_length=1000, default='none')
 
     def __str__(self):
         return self.user.username
--- app/bargain_box/user_profile/urls.py
+++ app/bargain_box/user_profile/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('view/', views.view_profile, name = 'view-profile'), # used to view the signed in user's profile
-    path('view/<int:pk>/', views.view_profile, name = 'post-detail-given-user-pk'), # used to view a non-signed in user's profile
-    path('edit/', views.edit_profile, name = 'edit-profile'),
-    path('delete/', views.delete_profile, name = 'delete-profile')
+    # The following route does not require the requesting client to be signed in.
+    # The following route is used to view a seller's profile.
+    path('view/<int:user_to_view_pk>/', views.view_profile, name = 'view-profile-given-user-pk'),
+
+    # The following routes require the requesting client to be signed in.
+    # The following routes are used to view, edit, and delete the signed in user's profile.
+    path('view/', views.view_my_profile, name = 'view-profile'),
+    path('edit/', views.edit_my_profile, name = 'edit-profile'),
+    path('delete/', views.delete_my_profile, name = 'delete-profile')
 ]
