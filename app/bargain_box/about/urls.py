a1c5dfb2e752ab4a35fe7d7f5faa31848f59d6e7
jasminSlootweg <105336332+jasminSlootweg@users.noreply.github.com>
2024-11-28 10:04:09 -0500

    Implemented the "How we work" page

--- app/bargain_box/about/urls.py
+++ app/bargain_box/about/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('meet_the_team/', views.meet_the_team, name='meet_the_team'),
+    path('how_we_work/', views.how_we_work, name='how_we_work' ),
 ]
--- app/bargain_box/about/views.py
+++ app/bargain_box/about/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def meet_the_team(request):
     return render(request, 'about/meet_the_team.html')
+
+def how_we_work(request):
+    return render(request, 'about/how_we_work.html')
