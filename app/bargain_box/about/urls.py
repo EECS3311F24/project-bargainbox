7bf23efa52ea310415efba120707b336bb1b5b31
jasminSlootweg <105336332+jasminSlootweg@users.noreply.github.com>
2024-11-28 10:30:36 -0500

    Implemented the Our Mission page

--- app/bargain_box/about/urls.py
+++ app/bargain_box/about/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('meet_the_team/', views.meet_the_team, name='meet_the_team'),
     path('how_we_work/', views.how_we_work, name='how_we_work' ),
+    path('our_mission/', views.our_mission, name='our_mission' ),
 ]
